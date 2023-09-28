9e2f440a240c623981a1fc88545c75813a6a9e16
Amirul Momenin <patainc@gmail.com>
2023-09-29 01:28:54 +0600

    Update ajax_add_cart.php

--- ajax_add_cart.php
+++ ajax_add_cart.php
@@ -1,10 +1,12 @@
 <?php
+
 session_start();
-require_once(  dirname(__FILE__)  .'/../../../../../../wp-load.php' );
+require_once(  dirname(__FILE__)  .'/../../../wp-load.php' );
+$id = "";
 
 WC()->session->set_customer_session_cookie( true );
-$id = WC()->cart->add_to_cart( $_POST['product_id'], $_POST['quantity'], $_POST['variation_id'], $attributes, 
-						array('player_id'=> $_POST['player_id']) );
+$id = WC()->cart->add_to_cart( $_REQUEST['product_id'], $_REQUEST['quantity'], $_REQUEST['variation_id'], $attributes, 
+						array('player_id'=> $_REQUEST['player_id']) );
 						
-			echo json_encode(array('status'=>'success','id'=>$id));			
+			echo json_encode(array('status'=>'success','id'=>$id));		
 ?>
